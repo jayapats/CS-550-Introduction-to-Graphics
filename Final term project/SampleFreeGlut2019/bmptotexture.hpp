885da315a9caddf6f19ed5b92879bc8e7a829e28
jayapats <56284010+jayapats@users.noreply.github.com>
2020-12-30 15:07:19 -0800

    Add files via upload

---
+++ Final term project/SampleFreeGlut2019/bmptotexture.hpp
@@ -0,0 +1,17 @@
+//
+//  bmptotexture.hpp
+//  ComputerGraphicsFinalProject
+//
+//  Created by Brandon Lee on 12/1/16.
+//  Copyright © 2016 Brandon Lee. All rights reserved.
+//
+
+#ifndef bmptotexture_hpp
+#define bmptotexture_hpp
+
+#include <stdio.h>
+#include <errno.h>
+
+unsigned char *BmpToTexture( char *filename, int *width, int *height );
+
+#endif /* bmptotexture_hpp */
